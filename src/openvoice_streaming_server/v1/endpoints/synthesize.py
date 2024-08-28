c18df44cdf053398d7d5e20ea08fb9b679244962
cyai <83634399+cyai@users.noreply.github.com>
2024-08-28 11:37:11 +0530

    temp: no cloning

--- src/openvoice_streaming_server/v1/endpoints/synthesize.py
+++ src/openvoice_streaming_server/v1/endpoints/synthesize.py
@@ -73,18 +73,18 @@ class WebSocketHandler:
                     f"Received text: {text}, speaker: {speaker}, language: {language}, speed: {speed}"
                 )
 
-                # cloned_audio_stream = self.clone_model.tts_stream(
-                #     text, speaker, language, speed
-                # )
-                async for audio_chunk in self.model.generate_audio_chunks(
+                cloned_audio_stream = self.clone_model.tts_stream(
                     text, speaker, language, speed
-                ):
-                    cloned_audio_stream = self.clone_model.tts_stream(
-                        audio_chunk, self.source_se, self.target_se
-                    )
-                    await send_audio_stream(websocket, cloned_audio_stream)
+                )
+                # async for audio_chunk in self.model.generate_audio_chunks(
+                #     text, speaker, language, speed
+                # ):
+                #     cloned_audio_stream = self.clone_model.tts_stream(
+                #         audio_chunk, self.source_se, self.target_se
+                #     )
+                #     await send_audio_stream(websocket, cloned_audio_stream)
 
-                # await send_audio_stream(websocket, cloned_audio_stream)
+                await send_audio_stream(websocket, cloned_audio_stream)
         except WebSocketDisconnect:
             await self.disconnect(websocket)
         except Exception as e:
